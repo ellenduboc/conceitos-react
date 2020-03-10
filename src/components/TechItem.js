25a3e06645806a0ddbe520f7608cb3eea948e971
Ellen Duboc <ellenduboc-fsa@hotmail.com>
2020-03-10 19:58:44 -0300

    React property

---
+++ src/components/TechItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function techItem({ tech, onDelete }) {
+  return (
+    <li>
+      {tech}
+      <button onClick={onDelete} type="button">Remover</button>
+    </li>
+  );
+}
+
+export default techItem;
